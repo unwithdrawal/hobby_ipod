921116776185a323f77298178756346b63420c40
unwithdrawal <mia000000chh@gmail.com>
2020-07-01 09:55:55 +0900

    Initial Commit

---
+++ hobby_ipod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hobby_ipod
+//
+//  Created by Sota Saito on 2020/07/01.
+//  Copyright © 2020 unwithdrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hobby_ipodTests/hobby_ipodTests.swift
@@ -0,0 +1,34 @@
+//
+//  hobby_ipodTests.swift
+//  hobby_ipodTests
+//
+//  Created by Sota Saito on 2020/07/01.
+//  Copyright © 2020 unwithdrawal. All rights reserved.
+//
+
+import XCTest
+@testable import hobby_ipod
+
+class hobby_ipodTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
